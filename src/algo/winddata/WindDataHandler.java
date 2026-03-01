fc302af561391d6f329511d1b132ab081fb9d63f
xepsuper <xepsuper111@gmail.com>
2026-03-02 00:20:09 +0100

    BETA V2 - Extracted data from the file - Created a method that calculates average windspeed

--- src/algo/winddata/WindDataHandler.java
+++ src/algo/winddata/WindDataHandler.java
@@ -4,13 +4,18 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 import java.util.*;
 
 /**
  * Retrieves wind data from a weather station file.
  */
 public class WindDataHandler {
+    Map<String,String> observationByDate = new HashMap<>();
+    ArrayList<String[]> rawData = new ArrayList<>();
+    private final DateTimeFormatter dateOnlyFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
 	/**
 	 * Load wind data from file.
 	 *
@@ -20,10 +25,25 @@ public class WindDataHandler {
 	public void loadData(String filePath) throws IOException {
 		List<String> fileData = Files.readAllLines(Paths.get(filePath));  //O(n)
 
-        Map<String,String> list = new HashMap<>();
-
         for(String line : fileData)
-        { if (line.trim().isEmpty() || line.startsWith("#")) {continue;}}
+        { if (line.trim().isEmpty() || line.startsWith("#")) {continue;}
+
+            String[] parts = line.split(";");
+            String dataStr = parts[0];
+            String timeStr = parts[1];
+
+            String[] dataRow = new String[6];
+            for(int i = 0; i<4; i++)
+            {
+                dataRow[0] = dataStr;
+                dataRow[1] = timeStr;
+                dataRow[i+2] = parts[2+i];
+            }
+            rawData.add(dataRow);
+
+
+
+        }
 
 
 
@@ -42,10 +62,32 @@ public class WindDataHandler {
 	 * @return average wind speed for each date, sorted by date
 	 */
 	public List<String> averageWindSpeed(LocalDate dateFrom, LocalDate dateTo) {
+        List<String> result = new ArrayList<String>();
+        DateTimeFormatter outputFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
 
-		//TODO: Implement method
-		return null;  //O(1)
-	}
+        LocalDate currentDate = dateFrom;
+        double sum = 0;
+        int count = 0;
+
+        while(!currentDate.isAfter(dateTo)) {
+            List<String[]> dayData = rawData;
+
+            for (String[] row : dayData) {
+                try {
+                    double speed = Double.parseDouble(row[4]);
+                    sum += speed;
+                    count++;
+                } catch (NumberFormatException e) {
+                }
+            }
+            Double averageWindSpeed = count > 0 ? sum / count : 0;
+            result.add(String.format("%s average wind speed: %.2f m/s ", currentDate.format(dateOnlyFormatter), averageWindSpeed));
+
+
+             //O(1)
+        }
+        return result;
+    }
 
 	/**
 	 * Search for percentage of approved values (for both wind speed and wind direction) for dates.
@@ -76,7 +118,6 @@ public class WindDataHandler {
 	 * @return highest wind speed for each date, sorted by date
 	 */
 	public List<String> highestWindSpeed(LocalDate dateFrom, LocalDate dateTo) {
-
 		//TODO: Implement method
 		return null;  //O(1)
 	}
